f3912e6dbb907f7d5cbc00cd1de410b654970aa9
Loay-Ghanem <loay@DESKTOP-HS6OB2L.localdomain>
2026-02-25 00:25:06 +0300

    [enhance] home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\BannerRepository;
 use Inertia\Inertia;
 
 class HomeController extends Controller
 {
+
+    public function __construct(
+        protected BannerRepository $bannerRepository
+    ) {}
+
     public function __invoke()
     {
-        return Inertia::render('home');
+        return Inertia::render('home', [
+            'banners' => $this->bannerRepository->getAllBanners(),
+        ]);
     }
 }
