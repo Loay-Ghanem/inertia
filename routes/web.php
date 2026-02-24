579ca8ad288b0186657a8f44f0c813c46ad5abc5
Loay-Ghanem <loay@DESKTOP-HS6OB2L.localdomain>
2026-02-25 00:25:33 +0300

    [add] contact and about routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
+Route::get('/about', AboutController::class)->name('about');
+Route::get('/contact', ContactController::class)->name('contact');
 
 require __DIR__.'/settings.php';
