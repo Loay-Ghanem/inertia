b361ccb1145ab2199230636767b95571c5e43445
Loay-Ghanem <loay@DESKTOP-HS6OB2L.localdomain>
2026-02-24 22:39:25 +0300

    [handle] user can access filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,16 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Enums\UserType;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable, SoftDeletes;
@@ -47,5 +50,16 @@ class User extends Authenticatable
             'two_factor_confirmed_at' => 'datetime',
             'user_type'               => UserType::class,
         ];
+    }
+
+    public function isAdmin (?User $user = null): bool
+    {
+        $user = $user ?? Auth::user();
+        return $user && $user->user_type === UserType::ADMIN;
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->isAdmin();
     }
 }
