e5fd153b3de171d7202dd761c77924933e8709fd
Loay-Ghanem <loay@DESKTOP-HS6OB2L.localdomain>
2026-02-28 03:36:40 +0300

    [publish] lang

---
+++ lang/ar/filament/category.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'category'         => 'الصنف',
+    'categories'       => 'الأصناف',
+    'main_category'    => 'الصنف الرئيسي',
+    'main_categories'  => 'الأصناف الرئيسية',
+    'sub_category'     => 'الصنف الفرعي',
+    'sub_categories'   => 'الأصناف الفرعية',
+    'slug'             => 'الرابط المختصر',
+    'title_ar'         => 'العنوان (بالعربية)',
+    'title_en'         => 'العنوان (بالإنجليزية)',
+    'parent'           => 'الصنف الرئيسي',
+];
---
+++ lang/en/filament/category.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'category'   => 'Category',
+    'categories' => 'Categories',
+    'main_category' => 'Main Category',
+    'main_categories' => 'Main Categories',
+    'sub_category' => 'Sub Category',
+    'sub_categories' => 'Sub Categories',
+    'slug' => 'Slug',
+    'title_ar' => 'Title (Arabic)',
+    'title_en' => 'Title (English)',
+    'parent' => 'Parent',
+];
